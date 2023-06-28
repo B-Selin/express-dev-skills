4561609c43c00743297537cb99b6d3555a0389d8
B-Selin <selin@bozkurt.ca>
2023-06-28 17:37:57 -0600

    imported skill database to controller, export index and show functions

---
+++ controllers/skills.js
@@ -0,0 +1,19 @@
+// we are accessing the skill file - importing
+const Skill = require('../models/skill');
+
+module.exports = {
+  index,
+  show
+};
+
+function index(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll()
+  });
+}
+
+function show(req, res) {
+  res.render('skills/show', {
+    skill: Skill.getOne(req.params.id),
+  })
+}
