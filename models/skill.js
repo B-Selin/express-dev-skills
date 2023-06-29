d74cd42c5242266e4617919b541aa3f7f6e97d85
B-Selin <selin@bozkurt.ca>
2023-06-28 21:54:54 -0600

    MVC flowchart

--- models/skill.js
+++ models/skill.js
@@ -2,7 +2,7 @@ const skills = [
   { id: 123456, skill: 'Quantum Banana Peeling', description: 'Can peel a banana in multiple parallel universes simultaneously.' },
   { id: 234567, skill: 'Professional Pillow Fluffer', description: 'Can fluff pillows to the perfect level of softness.' },
   { id: 345678, skill: 'Expert Bubble Wrap Popping', description: 'Possesses lightning-fast reflexes and can pop bubble wrap with unmatched precision.' },
-  { id: 456789, skill: 'Professional Unicorn Whisperer', description: 'Capable of communicating telepathically with unicorns and granting wishes with rainbow powers.' },
+  { id: 456789, skill: 'Professional Wild Animal Whisperer', description: 'Capable of communicating telepathically with bears and other fluffy dangerous wild animals and get pets.' },
   { id: 567890, skill: 'Master of Coffee Telekinesis', description: 'Can stir coffee from a distance using the power of the mind, ensuring a perfectly blended cup every time.' },
   { id: 678901, skill: 'Extreme Sock Matching', description: 'Has an uncanny ability to find and pair matching socks, even in the most chaotic laundry piles.' },
 ];
--- routes/skills.js
+++ routes/skills.js
@@ -5,10 +5,12 @@ var router = express.Router();
 // Require the controller that exports skills CRUD functions
 var skillsCtrl = require('../controllers/skills');
 
+
+module.exports = router;
+
 // GET /skills
 router.get('/', skillsCtrl.index);
 
 // GET /skills/:id
 router.get('/:id', skillsCtrl.show)
 
-module.exports = router;
