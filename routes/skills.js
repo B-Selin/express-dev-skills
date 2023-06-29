6ba123adbf199a4fcc27f58366f344f307f0a87f
B-Selin <selin@bozkurt.ca>
2023-06-28 20:18:49 -0600

    typos, titles, html texts

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// Require the controller that exports skills CRUD functions
+var skillsCtrl = require('../controllers/skills');
+
+// GET /skills
+router.get('/', skillsCtrl.index);
+
+// GET /skills/:id
+router.get('/:id', skillsCtrl.show)
 
 module.exports = router;
